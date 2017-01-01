c39bae18b0b0f532b2029a43f37f2d8c2faa13ad
Sourav Barua <sourav.shr6@hotmail.com>
2017-01-02 04:18:55 +0600

    completed patient view controller

--- MedicalCommunityProject/Areas/Doctors/Controllers/PatientViewController.cs
+++ MedicalCommunityProject/Areas/Doctors/Controllers/PatientViewController.cs
@@ -11,14 +11,22 @@ namespace MedicalCommunityProject.Areas.Doctors.Controllers
     public class PatientViewController : Controller
     {
         // GET: Doctors/PatientView
-        public ActionResult Index()
+        public ActionResult Index(String searchString)
         {
+            
             DoctorsBL dbl = new DoctorsBL();
             PatientsBL pbl = new PatientsBL();
             Doctor LoggedInDoc = dbl.getByUN(User.Identity.Name);
             //sorting desc by PatientSince Value
             List<Patient> patientsOfThisDoc = pbl.getPatientList(LoggedInDoc.DocID).OrderByDescending(o=>o.PatientSince).ToList();
 
+            ViewBag.Stag = searchString;
+
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                patientsOfThisDoc = patientsOfThisDoc.Where(o => o.FirstName.ToLower().Contains(searchString.ToLower()) || o.LastName.ToLower().Contains(searchString.ToLower()) || o.Contact.ToLower().Contains(searchString.ToLower()) || o.Email.Contains(searchString.ToLower())).ToList();
+            }
 
             return View("PatientList",patientsOfThisDoc);
         }
