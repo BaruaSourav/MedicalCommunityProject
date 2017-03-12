6df86c19fd64170462bdbcbaaa4981f07f94abd7
Sourav Barua <sourav.shr6@hotmail.com>
2017-03-12 18:40:10 +0600

    Slight Bug Fix and feature improvement

---
+++ MedicalCommunityProject/Areas/Patients/Controllers/AddAppointmentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedicalCommunityProject.Areas.Patients.Controllers
+{
+    public class AddAppointmentController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View("AddAppointment");
+        }
+
+
+    }
+}
