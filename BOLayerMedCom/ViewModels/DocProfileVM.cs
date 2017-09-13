7b3613ef14dc70e2eb7517536f161125cc10396f
Sourav Barua <sourav.shr6@hotmail.com>
2017-09-14 01:08:04 +0600

    BranchTest

---
+++ BOLayerMedCom/ViewModels/DocProfileVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOLayerMedCom.ViewModels
+{
+    public class DocProfileVM
+    {
+
+
+        [Key]
+        public int id { get; set; }
+
+        public String Name { get; set; }
+        public int Fees { get; set; }
+        public bool isOnline { get; set; }
+        public String PracticingAddress { get; set; }
+        public int totalPatients { get; set; }
+        public String spec { get; set; }
+
+
+
+
+    }
+}
